96090e6fd26688ec65ca637f438bd61471afab33
Fiona Feng <ffeng@ucsd.edu>
2021-01-14 23:50:20 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,7 +2,16 @@ $(document).ready(function() {
     initializePage();
 });
 
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
+
 function initializePage() {
+	$("a.thumbnail").click(projectClick);
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
